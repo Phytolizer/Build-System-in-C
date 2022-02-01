5aeccac868c458b7545114b701dd4ccbe6d38af1
Kyle Coffey <me@phytolizer.dev>
2022-02-01 08:56:08 -0600

    Actually compile a functioning program

--- build.c
+++ build.c
@@ -11,8 +11,6 @@
       "-Wpedantic", "-fno-strict-aliasing", "-ggdb", "-std=c11"
 #endif
 
-const char *toolchain[] = {"basm", "bme", "bmr", "debasm", "bdb", "basm2nasm"};
-
 #ifdef _WIN32
 void build_c_file(const char *input_path, const char *output_path) {
   (void)output_path;
@@ -27,20 +25,6 @@ void build_c_file(const char *input_path, const char *output_path) {
 int main() {
   MKDIRS("build", "bin");
 
-  FOREACH_ARRAY(const char *, tool, toolchain, {
-    printf("Building %s...\n", CONCAT(tool, ".c"));
-    build_c_file(CONCAT(tool, ".c"), PATH("build", "bin", tool));
-  });
-
-  MKDIRS("build", "examples");
-
-  FOREACH_FILE_IN_DIR(example, "examples", {
-    size_t n = strlen(example);
-    assert(n >= 4);
-    printf("Building %s...\n", example);
-    if (strcmp(example + n - 4, "basm") == 0) {
-      CMD(PATH("build", "bin", "basm"), "-g", PATH("examples", example),
-          PATH("build", CONCAT(example, ".bm")));
-    }
-  });
+  printf("Building %s...\n", "example.c");
+  build_c_file("example.c", PATH("build", "bin", "example"));
 }
